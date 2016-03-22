c7d169bd8f9b50b14b2d6563ead35797ed430f65
Mithgol the Webmaster <getgit@mithgol.ru>
2016-03-22 22:54:09 +0300

    read parameters from the command line; print help if they're missing

--- fido2twi.js
+++ fido2twi.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 
 var fido2twi = require('./f2t-core.js');
+var clog = console.log;
 
 var params = [].concat(process.argv);
 params.shift(); // 'node'
 params.shift(); // 'twi2fido'
 
-console.log( fido2twi() );
+if( params.length < 2 ){
+   clog('Usage:');
+   clog('   fido2twi loginName sourceArea');
+   clog('');
+   clog('Parameters:');
+   clog('');
+   clog('loginName   -- login name (screen name) of a microblog in Twitter');
+   clog('');
+   clog('sourceArea  -- areatag of an echomail area in Fidonet');
+   process.exit(1);
+}
+
+const loginName  = params[0];
+const sourceArea = params[1];
+
+console.log( fido2twi(loginName, sourceArea) );
