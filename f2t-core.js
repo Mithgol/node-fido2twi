21759ef7d23e005a48f94f6bdecde30c3dda2c8b
Mithgol the Webmaster <getgit@mithgol.ru>
2016-04-08 20:07:34 +0300

    read configuration

--- f2t-core.js
+++ f2t-core.js
@@ -1,1 +1,23 @@
-module.exports = () => 'Hello world.';
+var path = require('path');
+var simteconf = require('simteconf');
+
+module.exports = () => {
+   var confF2T = simteconf(
+      path.resolve(__dirname, 'fido2twi.config'),
+      { skipNames: ['//', '#'] }
+   );
+   // Read HPT areas:
+   var encodingHPT = confF2T.last('EncodingHPT') || 'utf8';
+   var areasHPT = simteconf(confF2T.last('AreasHPT'), {
+      encoding: encodingHPT,
+      skipNames: ['#'],
+      lowercase: false,
+      prefixGroups: [
+         'NetmailArea',
+         'BadArea',
+         'DupeArea',
+         'LocalArea',
+         'EchoArea'
+      ]
+   });
+};
