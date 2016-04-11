ae1fe411a577dd616be0c5d000eb874c54c91025
Mithgol the Webmaster <getgit@mithgol.ru>
2016-04-11 22:03:08 +0300

    define `getLastReadFromFile` for handling JSON-coded “last read” data

--- f2t-core.js
+++ f2t-core.js
@@ -1,6 +1,17 @@
+var fs = require('fs');
 var path = require('path');
 var fidoconfig = require('fidoconfig');
 var simteconf = require('simteconf');
+
+var getLastReadFromFile = filename => {
+   try {
+      var readData = fs.readFileSync(filename, {encoding: 'utf8'});
+      if( /^\s*$/.test(readData) ) return null;
+      return JSON.parse(readData);
+   } catch(e) {
+      return null;
+   }
+};
 
 module.exports = () => {
    var confF2T = simteconf(
