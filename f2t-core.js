e9113788fbc4e32022f14d6d41c3e9a5e639a854
Mithgol the Webmaster <getgit@mithgol.ru>
2016-04-12 04:11:33 +0300

    report echomail areas that are missing or passthrough

--- f2t-core.js
+++ f2t-core.js
@@ -1,19 +1,31 @@
 var fs = require('fs');
 var path = require('path');
+var cl = require('ciel');
 var fidoconfig = require('fidoconfig');
 var simteconf = require('simteconf');
+
+var quitOnError = (err, areaTag) => {
+   if( err.notFound ){
+      cl.fail(`The area ${areaTag} is not found.`);
+   } else if( err.passthrough ){
+      cl.fail(`The area ${areaTag} is a passthrough area.`);
+   } else cl.fail(`Unknown error reading area ${areaTag}.`);
+   process.exit(1);
+};
 
 var getLastReadFromFile = filename => {
    try {
       var readData = fs.readFileSync(filename, {encoding: 'utf8'});
       if( /^\s*$/.test(readData) ) return null;
-      return JSON.parse(readData);
+      var parsedData = JSON.parse(readData);
+      if(!( Array.isArray(parsedData) )) parsedData = [];
+      return parsedData;
    } catch(e) {
-      return null;
+      return [];
    }
 };
 
-module.exports = () => {
+module.exports = (loginName, sourceArea) => {
    var confF2T = simteconf(
       path.resolve(__dirname, 'fido2twi.config'),
       { skipNames: ['//', '#'] }
@@ -21,5 +33,13 @@ module.exports = () => {
    // Read HPT areas:
    var areas = fidoconfig.areas(confF2T.last('AreasHPT'), {
       encoding: confF2T.last('EncodingHPT') || 'utf8'
+   });
+
+   var lastRead = getLastReadFromFile(
+      path.resolve(__dirname, sourceArea + '.lastread.json')
+   );
+
+   areas.area(sourceArea, (err, areaData) => {
+      if( err ) return quitOnError(err, sourceArea);
    });
 };
